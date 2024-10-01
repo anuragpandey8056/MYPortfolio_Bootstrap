e1da95a71635cd2ca2644d29303032cf00c5b021
Anurag pandey <anuragpandey8056@gmail.com>
2024-10-01 20:11:48 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./APP";
+import App from "./App";
 // import "./r1.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 createRoot(document.getElementById("root")).render(<App />);
